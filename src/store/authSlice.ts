cb241d45cd5f7201b9564b489f2a8d1a9e8b4d9e
KasunGunarathna <kasunpradeepa.gunarathna@gmail.com>
2023-09-01 19:43:09 +0530

    fix: :bug: bugfix reload logout

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -1,28 +1,38 @@
-import { createSlice, PayloadAction,ThunkDispatch,AnyAction } from "@reduxjs/toolkit";
-import {  RootState } from "./store";
+import {
+  createSlice,
+  PayloadAction,
+  ThunkDispatch,
+  AnyAction,
+} from "@reduxjs/toolkit";
+import { RootState } from "./store";
 
 interface AuthState {
   token: string | null;
   isAuthenticated: boolean;
   nic: string | null;
-  tokenExpiry:number |null
+  tokenExpiry: number | null;
 }
 
 const initialState: AuthState = {
   token: null,
-  isAuthenticated: false,
+  isAuthenticated:
+    localStorage.getItem("isAuthenticated")?.toLowerCase?.() === "true" ||
+    false,
   nic: null,
-  tokenExpiry:null,
+  tokenExpiry: null,
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setToken: (state, action: PayloadAction<{ token: string; expiresIn: number }>) => {
+    setToken: (
+      state,
+      action: PayloadAction<{ token: string; expiresIn: number }>
+    ) => {
       state.token = action.payload.token;
       state.isAuthenticated = true;
-      state.tokenExpiry = Date.now() / 1000  + action.payload.expiresIn;
+      state.tokenExpiry = Date.now() / 1000 + action.payload.expiresIn;
     },
     setNic: (state, action) => {
       state.nic = action.payload;
@@ -30,14 +40,12 @@ const authSlice = createSlice({
     clearToken: (state) => {
       state.token = null;
       state.isAuthenticated = false;
-      state.tokenExpiry =null;
+      state.tokenExpiry = null;
     },
-    
   },
 });
 
-
-export const { setToken, clearToken ,setNic} = authSlice.actions;
+export const { setToken, clearToken, setNic } = authSlice.actions;
 
 export default authSlice.reducer;
 
---
+++ src/utils/tokenExpiration.ts
@@ -0,0 +1,15 @@
+import { clearToken } from '../store/authSlice';
+import { AppDispatch } from '../store/store';
+let timeoutId: NodeJS.Timeout | null = null;
+
+export const setTokenExpiration = (tokenExpiry: number, dispatch: AppDispatch) => {
+  clearTimeout(timeoutId!);
+
+  const currentTime = Date.now();
+  const timeUntilExpiration = tokenExpiry ;
+  console.log(timeUntilExpiration)
+  timeoutId = setTimeout(() => {
+   
+    dispatch(clearToken());
+  }, timeUntilExpiration);
+};
