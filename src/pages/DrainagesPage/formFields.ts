77ae89ebc2462b70e0539f42ad7f1189fe072628
KasunGunarathna <kasunpradeepa.gunarathna@gmail.com>
2023-09-20 20:56:44 +0530

    condition types

--- src/pages/DrainagesPage/formFields.ts
+++ src/pages/DrainagesPage/formFields.ts
@@ -1,4 +1,4 @@
-import { DrainageType, SideOfDrain } from "../../types/enum";
+import { DrainageType, SideOfDrain, SurfaceCondition } from "../../types/enum";
 import { FormField } from "../../types/types";
 
 export const fields: FormField[] = [
@@ -20,7 +20,12 @@ export const fields: FormField[] = [
     label: "Starting Point Latitude, Longitude",
   },
   { name: "end_point_location", label: "End Point Latitude, Longitude" },
-  { name: "condition", label: "Condition" },
+  {
+    name: "condition",
+    label: "Condition",
+    select: true,
+    options: Object.values(SurfaceCondition),
+  },
   { name: "length", label: "Length (meters)", type: "number" },
   { name: "width", label: "Width (meters)", type: "number" },
   // You can add more fields as needed
